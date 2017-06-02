7d9b0da6d09fa994fe2b9b7f1f9fea9f177f1c55
Dave Stone <Dave.Stone@nationstarmail.com>
2017-06-02 13:42:02 -0500

    Second Commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var renderOptions = { 
+                        title: 'Express II' 
+                    };
+
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', renderOptions);
 });
 
 module.exports = router;
